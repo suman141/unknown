3d6affaa5564756195c1a87b9b4a780065eae736
Suman <73638722+suman141@users.noreply.github.com>
2020-11-04 20:31:38 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 18, 2020',
+  birthdate: 'Jan 18, 2021',
   name: 'Koushiki'
 
 };
